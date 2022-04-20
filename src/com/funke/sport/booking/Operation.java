1aa6f330eadf330aa089f4fcbf78507205b157c1
Abolade Funke Victoria <agboluajevictoria@yahoo.com>
2022-04-20 01:28:34 +0100

    Creating lesson instance with preloaded data

--- src/com/funke/sport/booking/Operation.java
+++ src/com/funke/sport/booking/Operation.java
@@ -4,10 +4,28 @@ import java.util.*;
 
 public class Operation {
 
+    public static List<Lesson> lessonList = new ArrayList<>();
 
     public Operation() {
-
+        createLessons();
     }
+
+    public void createLessons(){
+        Lesson one = new Lesson("YOGA", 100, new ArrayList<>(), new ArrayList<>(), 0, 0);
+        Lesson two = new Lesson("ZUMBA", 200, new ArrayList<>(), new ArrayList<>(), 0, 0);
+        Lesson three = new Lesson("AQUACISE", 50, new ArrayList<>(), new ArrayList<>(), 0, 0);
+        Lesson four = new Lesson("BOX FIT", 200, new ArrayList<>(), new ArrayList<>(), 0, 0);
+        Lesson five = new Lesson("BODY BLITZ", 300, new ArrayList<>(), new ArrayList<>(), 0, 0);
+        Lesson six = new Lesson("DANCE", 150, new ArrayList<>(), new ArrayList<>(), 0, 0);
+
+        lessonList.add(one);
+        lessonList.add(two);
+        lessonList.add(three);
+        lessonList.add(four);
+        lessonList.add(five);
+        lessonList.add(six);
+    }
+
 
     public static void main(String[] args) {
         new Operation();
