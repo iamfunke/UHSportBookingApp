33a152954f1d2779dcf11a259f9339872d207816
Abolade Funke Victoria <agboluajevictoria@yahoo.com>
2022-04-20 01:32:07 +0100

    Student Registration and Dashboard display instance with preloaded data

--- src/com/funke/sport/booking/Operation.java
+++ src/com/funke/sport/booking/Operation.java
@@ -6,11 +6,16 @@ public class Operation {
 
     public static List<Lesson> lessonList = new ArrayList<>();
     public static List<Weekend> timeTable = new ArrayList<>();
+    public static List<Student> studentList = new ArrayList<>();
+
+    String saturday = "SATURDAY";
+    String sunday = "SUNDAY";
 
     public Operation() {
         createLessons();
         createTimeTable();
-
+        registerStudents();
+        showMenu();
     }
 
     public void createLessons(){
@@ -65,6 +70,67 @@ public class Operation {
         }
     }
 
+    public void registerStudents() {
+
+        Student one = new Student("YUSUF", new ArrayList<>(), new ArrayList<>());
+        Student two = new Student("JOHN", new ArrayList<>(), new ArrayList<>());
+        Student three = new Student("STEVEN", new ArrayList<>(), new ArrayList<>());
+        Student four = new Student("MARY", new ArrayList<>(), new ArrayList<>());
+        Student five = new Student("KHALID", new ArrayList<>(), new ArrayList<>());
+        Student six = new Student("JULIET", new ArrayList<>(), new ArrayList<>());
+        Student seven = new Student("ABRAHAM", new ArrayList<>(), new ArrayList<>());
+        Student eight = new Student("FATIMA", new ArrayList<>(), new ArrayList<>());
+        Student nine = new Student("ADAM", new ArrayList<>(), new ArrayList<>());
+        Student ten = new Student("LOVE", new ArrayList<>(), new ArrayList<>());
+
+        studentList.add(one);
+        studentList.add(two);
+        studentList.add(three);
+        studentList.add(four);
+        studentList.add(five);
+        studentList.add(six);
+        studentList.add(seven);
+        studentList.add(eight);
+        studentList.add(nine);
+        studentList.add(ten);
+    }
+
+    public void showMenu(){
+        while(true){
+            System.out.println();
+            System.out.println("Select Operation");
+            System.out.println("1: Login as student");
+            System.out.println("2: Generate report");
+            System.out.println("3: Show Timetable");
+            System.out.println("4: Exit");
+
+            System.out.print("INPUT: ");
+
+            Scanner input = new Scanner(System.in);
+            String select = input.next();
+
+            if(select.equals("1")){
+                login();
+            }
+            else if(select.equals("2")){
+                showReportMenu();
+            }
+            else if(select.equals("3")){
+                int count = 1;
+                for (Weekend weekend : timeTable){
+                    System.out.println("Week " + count + ": " + weekend);
+                    count++;
+                }
+            }
+            else if(select.equals("4")){
+                break;
+            }
+            else{
+                System.out.println("Invalid selection");
+            }
+        }
+
+    }
 
     public static void main(String[] args) {
         new Operation();
