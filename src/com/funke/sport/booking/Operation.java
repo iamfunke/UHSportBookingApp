08a532939fc528ec451c9cb547b7c26115bbd3fd
Abolade Funke Victoria <agboluajevictoria@yahoo.com>
2022-04-20 01:27:49 +0100

    Implementing the core operation and business logic

---
+++ src/com/funke/sport/booking/Operation.java
@@ -0,0 +1,16 @@
+package com.funke.sport.booking;
+
+import java.util.*;
+
+public class Operation {
+
+
+    public Operation() {
+
+    }
+
+    public static void main(String[] args) {
+        new Operation();
+    }
+}
+
